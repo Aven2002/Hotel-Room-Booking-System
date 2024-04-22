9ffdd32b402225c12fb87b76dbf5c3ff4b065184
AvenDing <avending2002@1utar.my>
2024-04-23 02:42:36 +0800

    Get Waiting Status

--- src/application/my/edu/utar/WaitingList.java
+++ src/application/my/edu/utar/WaitingList.java
@@ -8,6 +8,7 @@ public class WaitingList {
 	ArrayList<String> member = new ArrayList<String>();
 	ArrayList<String> normal = new ArrayList<String>();
 	private final Connection connection;
+	User user=new User();
 	
 	public WaitingList(Connection connection) {
 		this.connection=connection;
@@ -17,22 +18,36 @@ public class WaitingList {
 		
 	}
 	public void getWaiting(int userID) {
-        String query = "SELECT * FROM waiting_list WHERE userID = ?";
+		String query = "SELECT w.waitListID, u.username, w.created_at " +
+	               "FROM waiting_list w " +
+	               "INNER JOIN user_account u ON w.userID = u.userID " +
+	               "WHERE w.userID = ?";
 	try (PreparedStatement statement = connection.prepareStatement(query)) {
-           statement.setInt(1, userID);
-           ResultSet resultSet = statement.executeQuery();
-           
-           if (resultSet.next()) {
-               // User is in the waiting list, display the information
-               int waitingID = resultSet.getInt("waitListID");  
-               System.out.println("User is in the waiting list.");
-               System.out.println("Waiting ID: " + waitingID);
-           } else {
-               System.out.println("User is not in the waiting list.");
-           }
-       } catch (SQLException e) {
-           e.printStackTrace();
-       }
+	    statement.setInt(1, userID); // Use the userID obtained from login
+	    ResultSet resultSet = statement.executeQuery();
+
+	    if (resultSet.next()) {
+	        int waitingID = resultSet.getInt("waitListID");
+	        String username = resultSet.getString("username");
+	        Timestamp timestamp = resultSet.getTimestamp("created_at");
+
+	        System.out.println("\n==============================================");
+	        System.out.println("                Waiting Status                 ");
+	        System.out.println("==============================================");
+	        System.out.println("   Waiting ID   : " + waitingID);
+	        System.out.println("   Username     : " + username);
+	        System.out.println("   Created At   : " + timestamp);
+	        System.out.println("==============================================");
+	    } else {
+	    	System.out.println("\n==============================================");
+	        System.out.println("     No waiting record found for the user.");
+	        System.out.println("==============================================");
+	    }
+	    user.displayMenu();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+
    }
 	public void removeWaiting() {
 		
